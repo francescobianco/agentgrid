d11b23122d95fb36eea8df73283ab679d451f15e
francesco <info.francescobianco@gmail.com>
2026-05-11 16:06:32 +0200

    introducing env files for deployment

---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+[ ! -d "/opt/agentgrid" ] && git clone https://github.com/francescobianco/agentgrid /opt/agentgrid
+
+cd /opt/agentgrid || exit 1
+
+ls
+
+git pull --no-rebase
+
+#chmod 777 data/
+
+#cp -f compose.override.example compose.override.yml
+
+docker compose up -d --build --force-recreate
